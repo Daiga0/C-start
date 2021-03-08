d4a1977999c88083d6a35414621278273bc461de
Daiga0 <74953083+Daiga0@users.noreply.github.com>
2021-03-08 20:49:57 +0200

    Add files via upload

---
+++ C++.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+/* Ši programa pakelia įrašytą skaičių kvadratu */
+
+int x;
+int score = x * x;
+
+std::cout << "Įveskite skaičių: " << endl;
+
+std::cin >> x;
+
+std::cout << "Įvestas skaičius pakeltas kvadratu: " << score << endl;
+
+/* Ši programa sudeda įrašytus skaičius */
+
+int y;
+int a;
+int res = y + a;
+
+std::cout << "Įveskite skaičių: " << endl;
+
+std::cin >> y;
+
+std::cout << "Įveskite antrą skaičių: " << endl;
+
+std::cin >> a;
+
+std::cout << "Įvestų skaičių suma: " << res << endl;
+
+return 0;
+}
